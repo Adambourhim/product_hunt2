b88a3bff6fb366e10f4726197faae8e63dba9473
adambourhim <benbourhim7@gmail.com>
2021-03-31 12:27:50 +0300

    all comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./ComponentHeader/Header";
+import Main from "./ComponentMain/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
--- src/ComponentMain/Main.js
+++ src/ComponentMain/Main.js
@@ -1,24 +1,14 @@
-function Main(props) {
-  const { picture, name, time, id } = props;
+import axios from "axios";
+function Main() {
   return (
     <section className="sectionCenter">
-      <div className="idPosition">{id}</div>
-
       <div>
         <div>
-          <img src={picture} style={{ height: 100 }} alt="..." />
-        </div>
-
-        <div>
-          <div>
-            <h5>{name}</h5>
-            <p>
-              This is a wider card with supporting text below as a natural .
-            </p>
-            <p>
-              <small>Last updated {time}mins ago</small>
-            </p>
-          </div>
+          <h5>{name}</h5>
+          <p>This is a wider card with supporting text below as a natural .</p>
+          <p>
+            <small>Last updated {time}mins ago</small>
+          </p>
         </div>
       </div>
     </section>
