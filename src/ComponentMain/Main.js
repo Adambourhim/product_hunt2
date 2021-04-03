66891e5b172643b7e0bf61e5c838a5c36100bf88
adambourhim <benbourhim7@gmail.com>
2021-04-03 11:43:35 +0300

    comit5

--- src/ComponentMain/Main.js
+++ src/ComponentMain/Main.js
@@ -2,31 +2,28 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 function Main() {
   const url = "https://jsonplaceholder.typicode.com/posts";
-  const [post, setPost] = useState(null);
-  useEffect(() => {
-    axios.get(url).then((response) => {
-      setPost(response.data);
-      console.log(response.data);
+  const [post, setPost] = useState("");
+
+  const callApi = () => {
+    axios.get(url).then((res) => {
+      console.log(res);
+      setPost(res.headers.pragma);
     });
-  }, [url]);
-  if (post) {
-    return (
-      <section className="sectionCenter">
+  };
+  return (
+    <section className="sectionCenter">
+      <div>
         <div>
-          <div>
-            <h3>{post.body}</h3>
-            <p>
-              This is a wider card with supporting text below as a natural .
-            </p>
-            <p>
-              <small>Last updated time mins ago</small>
-            </p>
-          </div>
+          <h3>{post}</h3>
+          <button onClick={callApi}>Click me</button>
+          <p>This is a wider card with supporting text below as a natural .</p>
+          <p>
+            <small>Last updated time mins ago</small>
+          </p>
         </div>
-      </section>
-    );
-  }
-  return <div></div>;
+      </div>
+    </section>
+  );
 }
 
 export default Main;
