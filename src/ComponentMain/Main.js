dc6877912c3f2852e884599557b43fea0565c10c
adambourhim <benbourhim7@gmail.com>
2021-04-03 11:09:25 +0300

    comit2

--- src/ComponentMain/Main.js
+++ src/ComponentMain/Main.js
@@ -1,18 +1,31 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 function Main() {
-  return (
-    <section className="sectionCenter">
-      <div>
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setPost(response.data);
+    });
+  }, [url]);
+  if (post) {
+    return (
+      <section className="sectionCenter">
         <div>
-          <h3>Product N1</h3>
-          <p>This is a wider card with supporting text below as a natural .</p>
-          <p>
-            <small>Last updated time mins ago</small>
-          </p>
+          <div>
+            <h3>{post.name}</h3>
+            <p>
+              This is a wider card with supporting text below as a natural .
+            </p>
+            <p>
+              <small>Last updated time mins ago</small>
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+  return <div></div>;
 }
 
 export default Main;
